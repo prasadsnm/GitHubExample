a07008b14dfaa7526e6973d5231c0b9816b05dbe
vara prasad <gdv.prasadg@gmail.com>
2013-11-27 12:10:35 +0530

    Initial Commit

---
+++ GitHubExample/ClickAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ClickAppDelegate.h
+//  GitHubExample
+//
+//  Created by MAC on 27/11/13.
+//  Copyright (c) 2013 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ClickAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitHubExample/ClickViewController.h
@@ -0,0 +1,13 @@
+//
+//  ClickViewController.h
+//  GitHubExample
+//
+//  Created by MAC on 27/11/13.
+//  Copyright (c) 2013 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ClickViewController : UIViewController
+
+@end
